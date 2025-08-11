d9208e78b713b4a4f4d2db43ee3721fbe482a367
Jaye Chan <mb98237754@gmail.com>
2025-08-11 10:06:51 +0000

    any better or higher performance model then gemini-2.0-flash can be reco

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@gen-kit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash',
+  model: 'googleai/gemini-2.5-pro',
 });
